abe4504b70f533322d3dfa8a90d1b7809a4b4d0b
Akshat-gour <Akshatgour17@gmail.com>
2023-01-24 18:07:43 +0530

    initial commit

--- src/CoinTable.test.js
+++ src/CoinTable.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
-import CoinsTable from './../components/CoinsTable'
+import CoinsTable from '../components/CoinsTable'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Input value', () => {
--- src/banner.test.js
+++ src/banner.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render,screen } from '@testing-library/react';
-import Banner from '../components/Banner/Banner';
+import Banner from './components/Banner/Banner';
 
 describe('Banner', () => {
   it('should render a <div> with class "banner"', () => {
