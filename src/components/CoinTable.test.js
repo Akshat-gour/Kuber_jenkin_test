283cfb39b43041b51d4121f6006b2e8f49aa7bca
Akshat-gour <Akshatgour17@gmail.com>
2023-01-24 17:45:28 +0530

    initial commit

---
+++ src/components/CoinTable.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import CoinsTable from './CoinsTable'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('Input value', () => {
+    it('renders default state', () => {
+      render(<CoinsTable />)
+      const searchInput = screen.getByTestId('searchBox') 
+      expect(searchInput.value).toBe('') 
+    })
+
+    it('change input text field', () => {
+      render(<CoinsTable />)
+      const searchInput = screen.getByTestId('searchBox') 
+      fireEvent.change(searchInput, { target: { value: 'test' } })
+      expect(searchInput.value).toBe('test') 
+    })
+  })
